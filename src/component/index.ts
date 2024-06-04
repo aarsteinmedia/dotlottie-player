b24553a9ccc5c7add733f370303bff3d9dafbb1a
Johan Martin Aarstein <johan@aarstein.media>
2024-06-04 13:43:11 +0200

    bugfix

--- src/component/index.ts
+++ src/component/index.ts
@@ -257,9 +257,10 @@ export class DotLottiePlayer extends LitElement {
 
     // Loop
     let loop = !!this.loop
-    if (currentAnimationManifest.loop !== undefined) {
-      loop = !!currentAnimationManifest.loop
-    }
+    // This results in unwanted behavior, and is disabled
+    // if (currentAnimationManifest.loop !== undefined) {
+    //   loop = !!currentAnimationManifest.loop
+    // }
     if (currentAnimationSettings?.loop !== undefined) {
       loop = !!currentAnimationSettings.loop
     }
