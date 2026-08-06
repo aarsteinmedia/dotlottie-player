bdf1caa129f9e5bb4cb8265de1d751491e5360f3
Johan Martin Aarstein <johan@aarstein.media>
2026-08-06 22:24:45 +0200

    writeFiles

--- preview/writeFiles.ts
+++ preview/writeFiles.ts
@@ -5,15 +5,15 @@ import { fileURLToPath } from 'node:url'
 // List assets and write file
 const { url } = import.meta,
   __dirname = dirname(fileURLToPath(url)),
-  assets = readdirSync(resolve(__dirname, '..', 'assets'))
+  assets = readdirSync(resolve(
+    __dirname, '..', 'assets'
+  )).filter(file => /\.(?:json|lottie)$/i.test(file))
 
 try {
-  writeFileSync(
-    resolve(__dirname, 'files.js'),
+  writeFileSync(resolve(__dirname, 'files.js'),
     `const files = ${JSON.stringify(assets)}
 
-export default files`
-  )
+export default files`)
 } catch (error) {
   console.error(error)
 }
